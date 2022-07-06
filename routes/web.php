2b2a8c239000a49b5d98a8f22a4359cfef3490fe
Stalin Beltran <stalin.beltran2006@gmail.com>
2022-07-06 15:11:53 -0500

    corregido url, para que no use /public

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('public/students', StudentController::class);
+Route::resource('students', StudentController::class);
